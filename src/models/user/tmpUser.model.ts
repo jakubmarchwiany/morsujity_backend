dd2c2691480515f6c374590410bd901582bf7ab0
kvbik <xkvbikx@gmail.com>
2022-06-25 22:39:48 +0200

    added tmpUser

---
+++ src/models/user/tmpUser.model.ts
@@ -0,0 +1,26 @@
+import * as mongoose from "mongoose";
+
+import TmpUser from "./tmpUser.interface";
+
+const tmpUserSchema = new mongoose.Schema<TmpUser>({
+    email: String,
+    password: String,
+    pseudonym: String,
+    expireIn: { type: Date, default: new Date() },
+});
+
+if (process.env.ENV == "development")
+    tmpUserSchema.index(
+        { expireIn: 1 },
+        { expireAfterSeconds: parseInt(process.env.DEV_ACCOUNT_EXPIRE_AFTER!) }
+    );
+
+if (process.env.ENV == "production")
+    tmpUserSchema.index(
+        { expireIn: 1 },
+        { expireAfterSeconds: parseInt(process.env.PRO_ACCOUNT_EXPIRE_AFTER!) }
+    );
+
+const tmpUserModel = mongoose.model<TmpUser>("TmpUser", tmpUserSchema);
+
+export default tmpUserModel;
