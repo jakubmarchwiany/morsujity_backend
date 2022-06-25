c65243b8f1dd83840ac49d30f185976523ebf517
kvbik <xkvbikx@gmail.com>
2022-06-26 00:19:53 +0200

    added tryCatch when connection lost

--- src/utils/mailBot.ts
+++ src/utils/mailBot.ts
@@ -1,37 +1,45 @@
 "use strict";
+import * as bcrypt from "bcryptjs";
 import * as nodemailer from "nodemailer";
 import { Transporter } from "nodemailer";
-import * as bcrypt from "bcryptjs";
 
 import tmpHashModel from "../models/tmpHash/tmpHash.model";
-import TmpHash from "models/tmpHash/tmpHash.interface";
 import verificationMessage from "./verificationMessage";
 
 const { SERVER_HOST, SERVER_MAIL_PORT, SERVER_MAIL_USER, SERVER_MAIL_PASS } = process.env;
 
 class MailBot {
-    private transporter: Transporter;
+    private transporter!: Transporter;
     private tmpHash = tmpHashModel;
 
     constructor() {
-        this.transporter = nodemailer.createTransport({
-            host: SERVER_HOST,
-            port: parseInt(SERVER_MAIL_PORT!),
-            secure: true,
-            auth: {
-                user: SERVER_MAIL_USER,
-                pass: SERVER_MAIL_PASS,
-            },
-            // tls: {
-            //     rejectUnauthorized: false,
-            // },
-        });
-        console.log("Mail bot working");
+        this.createTransport();
+    }
+
+    private async createTransport() {
+        try {
+            this.transporter = nodemailer.createTransport({
+                host: SERVER_HOST,
+                port: parseInt(SERVER_MAIL_PORT!),
+                secure: true,
+                auth: {
+                    user: SERVER_MAIL_USER,
+                    pass: SERVER_MAIL_PASS,
+                },
+                // tls: {
+                //     rejectUnauthorized: false,
+                // },
+            });
+        } catch (error) {
+            setTimeout(() => {
+                this.createTransport();
+            }, 10000);
+        }
     }
 
     private async createTmpHash(targetMail: string, accountId: string) {
         let verificationHash = await bcrypt.hash(targetMail, 10);
-        verificationHash = verificationHash.replace("/", "k");
+        verificationHash = verificationHash.replace(/\//g, "k");
         await this.tmpHash.create({
             hash: verificationHash,
             accountRef: accountId,
@@ -42,7 +50,7 @@ class MailBot {
     public sendVerificationMail = async (targetMail: string, accountId: string) => {
         try {
             let hash = await this.createTmpHash(targetMail, accountId);
-            
+
             let message = verificationMessage(hash);
 
             let mailOptions = {
@@ -54,7 +62,9 @@ class MailBot {
 
             this.transporter.sendMail(mailOptions);
         } catch (error) {
-            console.log(error);
+            setTimeout(() => {
+                this.sendVerificationMail(targetMail, accountId);
+            }, 10000);
         }
     };
 }
