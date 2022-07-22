0a2cd2b3992f40c8923cb4f74950752b9d3fcd2d
xkvbikx <xkvbikx@gmail.com>
2022-07-22 20:27:25 +0200

    updated logginIn, RegisterUser logic and added createResetUserPasswordToken

--- src/controllers/authentication.controller.ts
+++ src/controllers/authentication.controller.ts
@@ -1,33 +1,39 @@
 import * as bcrypt from "bcryptjs";
+import * as crypto from "crypto";
 import * as express from "express";
 import * as jwt from "jsonwebtoken";
 
 import EmailVerificationNotFoundOrExpired from "../middleware/exceptions/EmailVerificationNotFoundOrExpired";
+import HttpException from "../middleware/exceptions/HttpException";
 import UserWithThatEmailAlreadyExistsException from "../middleware/exceptions/UserWithThatEmailAlreadyExistsException";
 import WrongCredentialsException from "../middleware/exceptions/WrongCredentialsException";
 
 import Controller from "../interfaces/controller.interface";
 
-import DataStoredInToken from "../models/token/dataStoredInToken.interface";
+import DataStoredInToken from "../models/authenticationToken/dataStoredInToken.interface";
 
-import tmpHashModel from "../models/tmpHash/tmpHash.model";
-import userModel from "../models/user/user.model";
-
+import passwordResetTokenModel from "../models/passwordResetToken/passwordResetToken.model";
 import tmpUserModel from "../models/user/tmpUser.model";
+import userModel from "../models/user/user.model";
 
 import MailBot from "../utils/mailBot";
 
-import TokenData from "models/token/tokenData.interface";
+import TokenData from "models/authenticationToken/tokenData.interface";
 import User from "models/user/user.interface";
-import HttpException from "../middleware/exceptions/HttpException";
-import registerUserSchema from "../middleware/schemas/registerUser";
+
+import emailTokenSchema from "../middleware/schemas/emailToken";
+import loginUserSchema, { loginUserData } from "../middleware/schemas/loginUser";
+import registerUserSchema, { registerUserData } from "../middleware/schemas/registerUser";
 import validate from "../middleware/validate.middleware";
+
+import catchError from "../utils/catchError";
+
 class AuthenticationController implements Controller {
     public path = "/auth";
     public router = express.Router();
     private user = userModel;
     private tmpUser = tmpUserModel;
-    private tmpHash = tmpHashModel;
+    private passwordResetToken = passwordResetTokenModel;
     private mailBot;
 
     constructor() {
@@ -36,19 +42,35 @@ class AuthenticationController implements Controller {
     }
 
     private initializeRoutes() {
-        this.router.post(`${this.path}/register`, validate(registerUserSchema), this.registration);
-        this.router.post(`${this.path}/login`, this.loggingIn);
+        this.router.post(
+            `${this.path}/register`,
+            validate(registerUserSchema),
+            catchError(this.registerUser)
+        );
+        this.router.get(
+            `${this.path}/verifyUserEmail/:token`,
+            validate(emailTokenSchema),
+            catchError(this.verifyUserEmail)
+        );
+        this.router.post(
+            `${this.path}/login`,
+            validate(loginUserSchema),
+            catchError(this.loggingIn)
+        );
         this.router.post(`${this.path}/logout`, this.loggingOut);
-        this.router.get(`${this.path}/verifyEmail/:hash`, this.verifyEmail);
+        this.router.post(
+            `${this.path}/resetPassword`,
+            catchError(this.createResetUserPasswordToken)
+        );
     }
 
-    private registration = async (
+    private registerUser = async (
         req: express.Request,
         res: express.Response,
         next: express.NextFunction
     ) => {
-        const userData: any = req.body; //to Do
-        if (await this.tmpUser.findOne({ email: userData.email })) {
+        const userData: registerUserData = req.body;
+        if (await this.user.findOne({ email: userData.email })) {
             next(new UserWithThatEmailAlreadyExistsException(userData.email));
         } else {
             const hashedPassword = await bcrypt.hash(userData.password, 10);
@@ -56,12 +78,32 @@ class AuthenticationController implements Controller {
                 ...userData,
                 password: hashedPassword,
             });
+            await this.mailBot.sendMailEmailUserVerification(user.email, user._id);
+            res.send({
+                message: "Udało się utworzyć konto. Mail z potwierdzeniem wysłany na email",
+            });
+        }
+    };
 
-            await this.mailBot.sendVerificationMail(user.email, user._id);
-            res.send({ message: "Udało się utworzyć konto" });
-            // const tokenData = this.createToken(user);
-            // response.setHeader("Set-Cookie", [this.createCookie(tokenData)]);
-            // response.send({user: user, token: tokenData.token});
+    private verifyUserEmail = async (
+        req: express.Request,
+        res: express.Response,
+        next: express.NextFunction
+    ) => {
+        let { token } = req.params;
+        let tmpUser = await this.tmpUser.findById(token);
+        if (tmpUser) {
+            let { email, password, pseudonym } = tmpUser;
+            await this.user.create({
+                email,
+                password,
+                pseudonym,
+            });
+
+            tmpUser.delete();
+            res.send({ message: "Udało się zweryfikować e-mail" });
+        } else {
+            next(new EmailVerificationNotFoundOrExpired());
         }
     };
 
@@ -70,13 +112,13 @@ class AuthenticationController implements Controller {
         res: express.Response,
         next: express.NextFunction
     ) => {
-        const logInData: any = req.body; //to Do
-        const user = await this.user.findOne({ email: logInData.email });
+        const logInData: loginUserData = req.body;
+        const user = await this.user.findOne({ email: logInData.email }, { status: 0 });
         if (user) {
             const isPasswordMatching = await bcrypt.compare(logInData.password, user.password);
             if (isPasswordMatching) {
                 user.password = undefined!;
-                const tokenData = this.createToken(user);
+                const tokenData = this.createAuthenticationToken(user);
                 res.setHeader("Set-Cookie", [this.createCookie(tokenData)]);
                 res.send({ user: user, token: tokenData.token, message: "Udało się zalogować" });
             } else {
@@ -87,46 +129,7 @@ class AuthenticationController implements Controller {
         }
     };
 
-    private loggingOut = (req: express.Request, res: express.Response) => {
-        res.setHeader("Set-Cookie", ["Authorization=;Max-age=0"]);
-        res.send({ message: "Udało się wylogować" });
-    };
-
-    private verifyEmail = async (
-        req: express.Request,
-        res: express.Response,
-        next: express.NextFunction
-    ) => {
-        try {
-            let { hash } = req.params;
-            let tmpHash = await this.tmpHash.findOne({ hash: hash });
-            if (tmpHash) {
-                let tmpUser = await this.tmpUser.findById(tmpHash.accountRef);
-                if (tmpUser) {
-                    let { email, password, pseudonym } = tmpUser;
-                    await this.user.create({
-                        email,
-                        password,
-                        pseudonym,
-                    });
-                }
-
-                tmpUser!.delete();
-                tmpHash.delete();
-                res.send({ message: "Udało się zweryfikować e-mail" });
-            } else {
-                next(new EmailVerificationNotFoundOrExpired());
-            }
-        } catch (error: any) {
-            next(new HttpException(500, error.message));
-        }
-    };
-
-    private createCookie(tokenData: TokenData) {
-        return `Authorization=${tokenData.token}; HttpOnly; Max-Age=${tokenData.expiresIn}`;
-    }
-
-    private createToken(user: User): TokenData {
+    private createAuthenticationToken(user: User): TokenData {
         const expiresIn = 60 * 60;
         const secret = process.env.JWT_SECRET;
         const dataStoredInToken: DataStoredInToken = {
@@ -138,6 +141,28 @@ class AuthenticationController implements Controller {
             token: jwt.sign(dataStoredInToken, secret!, { expiresIn }),
         };
     }
+
+    private createCookie(tokenData: TokenData) {
+        return `Authorization=${tokenData.token}; HttpOnly; Max-Age=${tokenData.expiresIn}`;
+    }
+
+    private loggingOut = (req: express.Request, res: express.Response) => {
+        res.setHeader("Set-Cookie", ["Authorization=;Max-age=0"]);
+        res.send({ message: "Udało się wylogować" });
+    };
+
+    private createResetUserPasswordToken = async (req: express.Request, res: express.Response) => {
+        let { email } = req.body;
+        let user = await this.user.findOne({ email: email });
+        if (user) {
+            let randomBytes = crypto.randomBytes(64).toString("hex");
+            let token = await bcrypt.hash(randomBytes, 10);
+            await this.passwordResetToken.create({ token, userId: user.id });
+            await this.mailBot.sendMailResetUserPassword(email, token);
+        }
+
+        res.send({ message: "Email resetujący hasło został wysłany" });
+    };
 }
 
 export default AuthenticationController;
