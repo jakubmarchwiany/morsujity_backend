84be8ae4d439dc7299959bab48c458fa9c0d2e90
xkvbikx <xkvbikx@gmail.com>
2022-07-28 18:21:38 +0200

    added authentication method

---
+++ src/middleware/exceptions/WrongAuthenticationTokenException.ts
@@ -0,0 +1,9 @@
+import HttpException from './HttpException';
+
+class WrongAuthenticationTokenException extends HttpException {
+  constructor() {
+    super(401, 'Token autoryzacji jest nieprawidłowy');
+  }
+}
+
+export default WrongAuthenticationTokenException;
