034cca1d074ae3f4cb9d9945f66c17056df94780
xkvbikx <xkvbikx@gmail.com>
2022-07-31 15:57:53 +0200

    +added getUserData method, static public folder

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,11 +1,23 @@
 import * as express from "express";
 import { Request, Response, NextFunction } from "express";
+import RequestWithUser from "../interfaces/requestWithUser.interface";
+import authMiddleware from "../middleware/auth.middleware";
 
 import Controller from "../interfaces/controller.interface";
+import userModel from "../models/user/user.model";
+import catchError from "../utils/catchError";
 import ImageBot from "../utils/ImageBot";
+
+let { ENV, DEF_USER_IMAGE_PATH, DEV_BACKEND_URL_ADDRESS, PRO_FRONT_URL_ADDRESS } = process.env;
+
+let frontUrlAddress: string;
+if (ENV == "development") frontUrlAddress = DEV_BACKEND_URL_ADDRESS!;
+else frontUrlAddress = PRO_FRONT_URL_ADDRESS!;
 
 class UserController implements Controller {
     public path = "/user";
+    public router: any = express.Router();
+    private user = userModel;
     private imageBot;
 
     constructor() {
@@ -14,6 +26,7 @@ class UserController implements Controller {
     }
 
     private initializeRoutes() {
+        this.router.post(`/getData`, authMiddleware, catchError(this.getUserData));
         this.router.post(
             `/newImage`,
             authMiddleware,
@@ -34,6 +47,12 @@ class UserController implements Controller {
         await this.user.findByIdAndUpdate(req.user.id, { image: fileName });
 
         res.send({ status: "success" });
+    };
+
+    private getUserData = async (req: RequestWithUser, res: Response, next: NextFunction) => {
+        let user = await this.user.findById(req.user.id, { password: 0 });
+        user!.image = `${frontUrlAddress}/${DEF_USER_IMAGE_PATH}/${user!.image}`;
+        res.send({ user: user });
     };
 }
 
